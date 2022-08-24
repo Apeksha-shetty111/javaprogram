050ae6651c908d7b6ee0ef47b2fcec2a56101790
Apeksha <95568236+Apeksha-shetty111@users.noreply.github.com>
2022-08-24 10:43:57 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class pattern{
+    public static void main(String args[]){
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // //variables 
+        // int a =10;
+        // int b=20;
+        // int sum = a+b;
+        // System.out.print(sum);
+//taking inputs from the user
+Scanner sc=new Scanner(System.in);
+System.out.print("enter the name");
+String name = sc.nextLine();
+System.out.print(name);
+
+        
+
+    }
+}
---
+++ sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class sum{
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+        int sum1;
+        System.out.print("enter the values of a and b");
+        a= sc.nextInt();
+        b=sc.nextInt();
+        sum1=a+b;
+        System.out.print(sum1);
+    }
+}
